e52a4a1bb3d0fab4d4ae34518717e1f36942d9d3
Grzegorz Rekowski <102578051+grekowski@users.noreply.github.com>
2022-04-29 08:35:25 +0100

    v2014

--- root/scripts/texts.js
+++
@@ -1,8 +0,0 @@
-var t1 = "<span style=\"margin-top: 16px; color: #c9c89a; font: 9pt Tahoma, Arial, Verdana, Helvetica, sans-serif; letter-spacing: 0.8pt;\">";
-var t2 = "";
-var t3 = "<h5>Scripture quotations</h5><p>Scriptures taken from the New King James Version&reg;. Copyright &copy; 1982 by Thomas Nelson, Inc. All rights reserved.</p>";
-var t4 = "<h5>Website layout</h5>";
-var t5 = "<p>Copyright &copy; - 2011 by Newcastle West Bible Fellowship. All rights reserved.<br />If you would like to use any materials from this website, please contact us.</p>";
-var t6 = "<h5>Messages online</h5><p>Access to <a href=\"http://www.ncwbiblefellowship.com/messages\">messages online</a> is protected by a password. If You don't know it, contact us to request your password.</p>"
-var t7 = "<div id=\"fb-root\"></div><script src=\"http://connect.facebook.net/en_US/all.js#xfbml=1\"></script>";
-var t8 = "";
