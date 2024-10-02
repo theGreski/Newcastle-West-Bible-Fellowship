4e981a1e428830797d1c69f9c9d4637c52a62796
Grzegorz Rekowski <102578051+theGreski@users.noreply.github.com>
2024-10-02 15:49:58 +0100

    debug

--- root/static/scripts.js
+++ root/static/scripts.js
@@ -7,12 +7,12 @@ function scrollEvent(includeTopPos=false) {
 
     if ($(window).width()) {
         //$("div[data-parallax='true']").each(function() {
-        $.each(window.parallaxElements,function(){
+        $.each(window.parallaxElements, function() {
             //console.log( $(this).data("parallax") );
             const bottomPos = parseInt($(this).offset().top - scroll + $(this).outerHeight());
             window.screen = parseInt($(this).outerHeight());
             const toppos = parseInt($(this).offset().top - scroll);
-            if (includeTopPos == true && toppos > screen + 20) {
+            if (includeTopPos == true && toppos > window.screen + 20) {
                 return;
             }
 
@@ -38,9 +38,16 @@ function draw() {
 // when address bar hides on mobile, it will not resize
 // recalculate only on orientationchange
 //
-function calcVH(id) {
-    const el = document.getElementById(id)
-    const vH = Math.max(el.offsetHeight);
+function calcVH(id, fullPageHeight=false) {
+    const el = document.getElementById(id);
+    let vH;
+
+    if (fullPageHeight) {
+        vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    } else {
+        vH = Math.max(el.offsetHeight);
+    }
+    
     el.setAttribute("style", "height:" + vH + "px;");
 }
 
